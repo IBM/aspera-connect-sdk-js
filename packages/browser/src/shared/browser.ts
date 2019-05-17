816c8724566cd3c395dcf5096ea7875fb0f67dd3
David Wosk <dwosk@us.ibm.com>
2019-05-17 15:06:27 -0700

    lint: Fix linting issues

--- packages/browser/src/shared/browser.ts
+++ packages/browser/src/shared/browser.ts
@@ -1,20 +1,20 @@
 let ua = typeof navigator !== 'undefined' ? navigator.userAgent : '';
 
-let check_safari = function (ua: any, minver: number) {
-  var match = ua.match(/(?:Version)[\/](\d+(\.\d+)?)/i);
-  var ver = parseInt((match && match.length > 1 && match[1] || "0"));
+let checkSafari = function (ua: any, minver: number) {
+  let match = ua.match(/(?:Version)[\/](\d+(\.\d+)?)/i);
+  let ver = parseInt((match && match.length > 1 && match[1] || '0'), 10);
   return (ver >= minver);
 };
 
-let check_edge = function (ua: any, minver: number) {
-  var match = ua.match(/(?:Edge)[\/](\d+(\.\d+)?)/i);
-  var ver = parseInt((match && match.length > 1 && match[1] || "0"));
+let checkEdge = function (ua: any, minver: number) {
+  let match = ua.match(/(?:Edge)[\/](\d+(\.\d+)?)/i);
+  let ver = parseInt((match && match.length > 1 && match[1] || '0'), 10);
   return (ver >= minver);
 };
 
-let check_firefox = function (ua: any, minver: number) {
-  var match = ua.match(/(?:Firefox)[\/](\d+(\.\d+)?)/i);
-  var ver = parseInt((match && match.length > 1 && match[1] || "0"));
+let checkFirefox = function (ua: any, minver: number) {
+  let match = ua.match(/(?:Firefox)[\/](\d+(\.\d+)?)/i);
+  let ver = parseInt((match && match.length > 1 && match[1] || '0'), 10);
   return (ver >= minver);
 };
 
@@ -22,10 +22,10 @@ export default {
   OPERA: /opera|opr/i.test(ua) && !/edge/i.test(ua),
   IE: /msie|trident/i.test(ua) && !/edge/i.test(ua),
   CHROME: /chrome|crios|crmo/i.test(ua) && !/opera|opr/i.test(ua) && !/edge/i.test(ua),
-  FIREFOX: /firefox|iceweasel/i.test(ua) && !/edge/i.test(ua) && check_firefox(ua, 50),
-  FIREFOX_LEGACY: /firefox|iceweasel/i.test(ua) && !/edge/i.test(ua) && !check_firefox(ua, 50),
-  EDGE_WITH_EXTENSION: /edge/i.test(ua) && check_edge(ua, 14),
-  EDGE_LEGACY: /edge/i.test(ua) && !check_edge(ua, 14),
+  FIREFOX: /firefox|iceweasel/i.test(ua) && !/edge/i.test(ua) && checkFirefox(ua, 50),
+  FIREFOX_LEGACY: /firefox|iceweasel/i.test(ua) && !/edge/i.test(ua) && !checkFirefox(ua, 50),
+  EDGE_WITH_EXTENSION: /edge/i.test(ua) && checkEdge(ua, 14),
+  EDGE_LEGACY: /edge/i.test(ua) && !checkEdge(ua, 14),
   SAFARI: /safari/i.test(ua) && !/chrome|crios|crmo/i.test(ua) && !/edge/i.test(ua),
-  SAFARI_NO_NPAPI: /safari/i.test(ua) && !/chrome|crios|crmo/i.test(ua) && !/edge/i.test(ua) && check_safari(ua, 10)
+  SAFARI_NO_NPAPI: /safari/i.test(ua) && !/chrome|crios|crmo/i.test(ua) && !/edge/i.test(ua) && checkSafari(ua, 10)
 };
