7f9f12d7708b94b0579edb8be330e5c21f7bdd97
DAVID WOSK <dwosk@us.ibm.com>
2021-04-30 11:51:01 -0700

    build: remove connectinstaller-4.js (#52)

--- packages/browser/webpack.config.js
+++ packages/browser/webpack.config.js
@@ -24,16 +24,6 @@ let bannerText = `  Revision: ${version}-${branch !== 'main' ? (branch + '-') : 
   http://www.asperasoft.com
   Copyright IBM Corp. 2008, ${new Date().getFullYear()}`
 
-// For backwards-compatibility keep connectinstaller-4.js files. Just put in version and description.
-const emptyFileText = `/*
-${bannerText}
-
-  NOTE: This file is expected to be empty. Functionality has been bundled into asperaweb-4.js and asperaweb-4.min.js.
-*/`;
-['connectinstaller-4.js', 'connectinstaller-4.min.js'].forEach(filename => {
-  fs.outputFile(path.join(__dirname, 'build', filename), emptyFileText);
-});
-
 const plugins = [
   new webpack.BannerPlugin({
     banner: () => { return bannerText }
