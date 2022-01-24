8c07a2b7d2f8e5d43dc49bbf837bfbf7512f14fd
David Wosk <dwosk@us.ibm.com>
2022-01-23 21:42:49 -0800

    fix: update dependencies

--- test/config/karma.integration.conf.js
+++ test/config/karma.integration.conf.js
@@ -1,6 +1,3 @@
-const puppeteer = require('puppeteer');
-process.env.CHROME_BIN = process.env.CHROME_BIN || puppeteer.executablePath();
-
 module.exports = function(config) {
   config.set({
     basePath: process.cwd(),
