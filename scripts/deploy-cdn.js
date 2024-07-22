004fb339350347cd1560a57d15ee0d68be9ad5d1
Jordy Herrera Mussio <jordyhm@outlook.com>
2024-07-22 16:32:52 -0600

    ci: use custom script instead of S3 deploy GitHub action (#71)

---
+++ scripts/deploy-cdn.js
@@ -0,0 +1,11 @@
+const { execSync } = require('child_process');
+const args = require('minimist')(process.argv.slice(4));
+
+const { folder, distId, bucketName, packageName, tagName } = args;
+const bucketPath = `${bucketName}/${packageName}/${tagName}`;
+
+// Sync to s3 and invalidate
+const syncCommand = `aws s3 sync . s3://${bucketPath} --delete --acl public-read`
+const invalidationCommand = `aws cloudfront create-invalidation --distribution-id ${distId} --paths ${bucketPath}/*`
+execSync(syncCommand, { stdio: 'inherit', cwd: folder });
+execSync(invalidationCommand, { stdio: 'inherit', cwd: folder });
