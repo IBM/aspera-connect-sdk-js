6b9a9e41dc4849f92d239fc8cb776d249ba16655
Jordy Herrera Mussio <jordyhm@ibm.com>
2024-09-12 17:36:35 -0600

    fix: replace double quotes with single quotes

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -5,8 +5,7 @@ const replace = require("gulp-replace");
 gulp.task("default", () => {
   return gulp
     .src("./dist/*.html")
-    .pipe(replace('type="module" ', ''))
-    .pipe(replace('crossorigin ', ''))
+    .pipe(replace('type="module"', "type='module'"))
     .pipe(replace('.js"></script>', '.js" inline></script>'))
     .pipe(replace('.css">', '.css" inline>'))
     .pipe(
